02d0ba65f8fc1acb8b24ee581c34be9e86eb1651
vincenzo722 <74825997+vincenzo722@users.noreply.github.com>
2026-09-17 14:31:03 +0200

    Set Visita Clienti v2 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "it.vincenzo.visitaclienti"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 20
+        versionName = "2.0.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
